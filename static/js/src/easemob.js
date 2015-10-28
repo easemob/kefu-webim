c1ec5f09a8f83476564bc034c2720384d899e821
stupidkiller <ooyt@qq.com>
2015-10-28 19:13:59 +0800

    update

--- static/js/src/easemob.js
+++ static/js/src/easemob.js
@@ -270,7 +270,7 @@
 
     //append easemob.utils.js
     var script = document.createElement('script');
-    script.src = config.domain + 'webim/easemob.utils.js';
+    script.src = config.domain + 'webim/static/js/easemob.utils.js';
     (document.head || document.getElementsByTagName('head')[0]).appendChild(script);
     if(script.readyState) {
         script.onreadystatechange = function() {
