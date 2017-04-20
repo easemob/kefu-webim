9160095d2fb4df82bfa126252166065d2f6abcc5
曹云扬 <caoyy@easemob.com>
2017-04-20 10:50:51 +0800

    ver

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ todo: delete file_download.png
 
 var debug = false;
 const TEMPLATE_DATA = {
-	WEBIM_PLUGIN_VERSION: '43.16.001'
+	WEBIM_PLUGIN_VERSION: '43.16.002'
 };
 
 const gulp = require('gulp');
