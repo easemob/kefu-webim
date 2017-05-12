5a13b6a9c8eb9144b7575301296bd8e454a4222c
曹云扬 <caoyy@easemob.com>
2017-05-12 23:16:27 +0800

    bugfix|【网页插件】每次调用API都会重复在window上绑定事件 http://j1.private.easemob.com/browse/CLOUD-6341

--- src/js/app/modules/apiTransfer.js
+++ src/js/app/modules/apiTransfer.js
@@ -1,32 +1,56 @@
-easemobim.api = function (apiName, data, success, error) {
-	//cache
-	easemobim.api[apiName] = easemobim.api[apiName] || {};
+(function(root){
+	var cached = {};
+	var apiTransfer;
 
-	var ts = new Date().getTime();
-	easemobim.api[apiName][ts] = {
-		success: success,
-		error: error
-	};
-	easemobim.getData
-		.send({
+	root.api = api;
+	root.initApiTransfer = initApiTransfer;
+
+	function initApiTransfer(){
+		apiTransfer = new easemobim.Transfer('cross-origin-iframe', 'data', true);
+
+		apiTransfer.listen(function (msg) {
+			var apiName = msg.call;
+			var timestamp = msg.timespan;
+			var isSuccess = msg.status === 0;
+			var callbacks;
+			var successCallback;
+			var errorCallback;
+
+			if (cached[apiName] && cached[apiName][timestamp]) {
+
+				callbacks = cached[apiName][timestamp];
+				delete cached[apiName][timestamp];
+
+				successCallback = callbacks.success;
+				errorCallback = callbacks.error;
+
+				if (isSuccess) {
+					typeof successCallback === 'function' && successCallback(msg);
+				}
+				else {
+					typeof errorCallback === 'function' && errorCallback(msg);
+				}
+			}
+		}, ['api']);
+	}
+
+	function api(apiName, data, success, error) {
+		var ts = new Date().getTime();
+
+		//cache
+		cached[apiName] = cached[apiName] || {};
+
+		cached[apiName][ts] = {
+			success: success,
+			error: error
+		};
+
+		apiTransfer.send({
 			api: apiName,
 			data: data,
 			timespan: ts,
 			// 标记postMessage使用object，47.9 增加
 			useObject: true
-		})
-		.listen(function (msg) {
-			if (easemobim.api[msg.call] && easemobim.api[msg.call][msg.timespan]) {
-
-				var callback = easemobim.api[msg.call][msg.timespan];
-				delete easemobim.api[msg.call][msg.timespan];
-
-				if (msg.status !== 0) {
-					typeof callback.error === 'function' && callback.error(msg);
-				}
-				else {
-					typeof callback.success === 'function' && callback.success(msg);
-				}
-			}
-		}, ['api']);
-};
+		});
+	};
+}(easemobim));
