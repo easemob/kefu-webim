4981cffd6929ebd5d13c1b4050f5db29bc484912
刘腾 <412454153@qq.com>
2017-05-26 15:55:11 +0800

    访客端h5在微信中能设置title    http://j1.private.easemob.com/browse/CLOUD-5525

--- src/js/app/modules/chat.js
+++ src/js/app/modules/chat.js
@@ -1225,6 +1225,11 @@
 					utils.removeClass(doms.sendFileBtn, 'hide');
 				}
 
+				// h5title设置
+				if(config.ui.H5Title.enabled){
+					document.title = config.ui.H5Title.content;
+				}
+
 				// 静音按钮
 				window.HTMLAudioElement
 					&& !utils.isMobile
