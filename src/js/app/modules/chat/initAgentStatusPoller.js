6bbe96f4a661b76f8147efea8ec73c411705fb16
曹云扬 <caoyy@easemob.com>
2017-06-22 14:35:57 +0800

    bugfix|不获取坐席在线状态 CLOUD-7101

--- src/js/app/modules/chat/initAgentStatusPoller.js
+++ src/js/app/modules/chat/initAgentStatusPoller.js
@@ -6,66 +6,55 @@ app.initAgentStatePoller = (function(_const,  utils, profile, eventListener, api
 		var topBar = document.querySelector('.em-widget-header');
 		$agentStatusText = topBar.querySelector('.em-header-status-text');
 
-		eventListener.add(_const.SYSTEM_EVENT.SESSION_OPENED, startOrStopPollAgentState);
-		eventListener.add(_const.SYSTEM_EVENT.SESSION_TRANSFERED, startOrStopPollAgentState);
-		eventListener.add(_const.SYSTEM_EVENT.SESSION_CLOSED, startOrStopPollAgentState);
-		eventListener.add(_const.SYSTEM_EVENT.SESSION_TRANSFERING, startOrStopPollAgentState);
-		eventListener.add(_const.SYSTEM_EVENT.SESSION_RESTORED, startOrStopPollAgentState);
+		// 开始轮询坐席状态
+		_timerHandler = setInterval(function (){
+			var officialAccount = profile.currentOfficialAccount;
+			_setAgentStatus(officialAccount);
+		}, 5000);
+
+		eventListener.add(_const.SYSTEM_EVENT.SESSION_OPENED, _setAgentStatus);
+		eventListener.add(_const.SYSTEM_EVENT.SESSION_TRANSFERED, _setAgentStatus);
+		eventListener.add(_const.SYSTEM_EVENT.SESSION_CLOSED, _setAgentStatus);
+		eventListener.add(_const.SYSTEM_EVENT.SESSION_TRANSFERING, _setAgentStatus);
+		eventListener.add(_const.SYSTEM_EVENT.SESSION_RESTORED, _setAgentStatus);
+		eventListener.add(_const.SYSTEM_EVENT.OFFICIAL_ACCOUNT_SWITCHED, function (officialAccount){
+			_update(officialAccount.status);
+		});
 	};
 
-	function _start() {
-		if (_timerHandler) return;
+	function _setAgentStatus(officialAccount) {
+		if (
+			officialAccount !== profile.currentOfficialAccount
+			|| !officialAccount
+			|| !profile.isChatWindowOpen
+			|| utils.isBrowserMinimized()
+		) return;
 
-		// start to poll
-		_timerHandler = setInterval(function () {
-			_update();
-		}, 5000);
-	}
-
-	function _stop() {
-		_timerHandler = clearInterval(_timerHandler);
-		$agentStatusText.innerText = '';
-	}
-
-	function _update() {
-		var officialAccount = profile.currentOfficialAccount || {};
 		var agentId = officialAccount.agentId;
 		var agentType = officialAccount.agentType;
 		var isSessionOpen = officialAccount.isSessionOpen;
 
-		if (
+		if (agentType === _const.AGENT_ROLE.ROBOT){
+			// 机器人不去轮询，显示为在线
+			_update('Online');
+		}
+		else if (
 			!profile.nickNameOption
 			|| !agentId
-			|| profile.isChatWindowOpen
-			|| utils.isBrowserMinimized()
-		) return;
-
-		if (!isSessionOpen){
-			$agentStatusText.innerText = '';
-		}
-		else if (agentType === _const.AGENT_ROLE.ROBOT){
-			// 机器人不去轮询，显示为在线
-			$agentStatusText.innerText = _const.agentStatusText.Online;
+			|| !isSessionOpen
+		){
+			_update(null);
 		}
 		else {
-			apiHelper.getAgentStatus(agentId).then(function (state) {
-				if (state) {
-					$agentStatusText.innerText = _const.agentStatusText[state];
-				}
+			apiHelper.getAgentStatus(agentId).then(function (status){
+				officialAccount.agentState = status;
+				_update(status);
 			});
 		}
 	}
 
-	function startOrStopPollAgentState(officialAccount, event){
-		if (officialAccount !== profile.currentOfficialAccount) return;
-
-		var sessionState = officialAccount.sessionState;
-
-		if (sessionState === _const.SESSION_STATE.PROCESSING){
-			_start();
-		}
-		else{
-			_stop();
-		}
+	function _update(status){
+		var agentStatusText = _const.agentStatusText[status || 'Other'];
+		$agentStatusText.innerText = agentStatusText;
 	}
 }(easemobim._const, easemobim.utils, app.profile, app.eventListener, app.apiHelper));
