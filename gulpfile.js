d8df623c3409e77fb434b726c440d23a8745afc1
liuteng <412454153@qq.com>
2017-09-07 12:16:20 +0800

    ver

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ todo: delete file_download.png
 
 var debug = false;
 const TEMPLATE_DATA = {
-	WEBIM_PLUGIN_VERSION: '43.15.5'
+	WEBIM_PLUGIN_VERSION: 'citic.43.15.5.1'
 };
 
 const gulp = require('gulp');
