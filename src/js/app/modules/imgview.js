79feacce177eb7873700a5325bfcdef07a6f40fd
刘腾 <412454153@qq.com>
2017-10-31 17:34:13 +0800

    手机端查看图片优化 CLOUD-6440

--- src/js/app/modules/imgview.js
+++ src/js/app/modules/imgview.js
@@ -4,22 +4,34 @@ var profile = require("./tools/profile");
 
 var img;
 var imgWrapper;
+var iosLoadTip;
+var androidLoadTip;
+var imgSrc;
 var _init = _.once(function(){
 	imgWrapper = document.querySelector("div.img-view");
 	img = imgWrapper.querySelector("img");
-
+	iosLoadTip = imgWrapper.querySelector("span.ios-load");
+	androidLoadTip = imgWrapper.querySelector("a.android-load");
+	utils.isAndroid && utils.removeClass(androidLoadTip, "hide");
+	utils.isIOS && utils.removeClass(iosLoadTip, "hide");
 	utils.on(imgWrapper, "click", function(){
 		utils.addClass(imgWrapper, "hide");
 	}, false);
+	!("download" in document.createElement("a")) && utils.on(androidLoadTip, "click", function(){
+		window.location = imgSrc;
+		return false;
+	});
+
 });
 
 module.exports = {
 	show: function(url){
+		imgSrc = url;
 		_init();
 		if(utils.isTop || utils.isMobile){
-			img.setAttribute("src", url);
+			img.src = url;
+			androidLoadTip.href = url;
 			utils.removeClass(imgWrapper, "hide");
-
 		}
 		else{
 			transfer.send({
