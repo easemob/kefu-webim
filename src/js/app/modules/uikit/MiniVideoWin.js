48f7c70523b8e981abd6efb48ed9134ad2e49d20
曹云扬 <caoyy@easemob.com>
2017-11-16 10:41:28 +0800

    Safari 视频通话功能适配 CLOUD-7049

--- src/js/app/modules/uikit/MiniVideoWin.js
+++ src/js/app/modules/uikit/MiniVideoWin.js
@@ -4,8 +4,9 @@ var _const = require("../../../common/const");
 var template = [
 	"<div class=\"mini-video-window\">",
 	"<p class=\"nickname\"></p>",
-	"<video muted autoplay class=\"main-video\"></video>",
-	"<video muted autoplay class=\"no-audio-video hide\"></video>",
+	"<video muted autoplay playsinline class=\"main-video\"></video>",
+	"<video muted autoplay playsinline class=\"no-audio-video hide\"></video>",
+	"<span class=\"play-button hide\"><i class=\"button-icon icon-star\"></i></span>",
 	"</div>",
 ].join("");
 
@@ -24,10 +25,17 @@ module.exports = MiniVideoWin = function(option){
 			streams: _.compact([me.stream, me.noAudioStream]),
 		});
 	};
+	this.eventHandler2 = function(e){
+		me.videoDom && me.videoDom.play();
+		me.noAudioVideoDom && me.noAudioVideoDom.play();
+		utils.addClass(me.playButtonDom, "hide");
+		e.stopPropagation();
+	};
 
 	this.dom = utils.createElementFromHTML(template);
 	this.nicknameDom = this.dom.querySelector(".nickname");
 	this.videoDom = this.dom.querySelector(".main-video");
+	this.playButtonDom = this.dom.querySelector(".play-button");
 	this.noAudioVideoDom = this.dom.querySelector(".no-audio-video");
 	this.parentContainer.appendChild(this.dom);
 
@@ -37,10 +45,12 @@ module.exports = MiniVideoWin = function(option){
 	this.updateStream(stream);
 
 	utils.on(this.dom, "click", this.eventHandler);
+	utils.on(this.playButtonDom, "click", this.eventHandler2);
 };
 
 MiniVideoWin.prototype.destroy = function(){
 	utils.off(this.dom, "click", this.eventHandler);
+	utils.off(this.playButtonDom, "click", this.eventHandler2);
 	this.dom.remove();
 };
 
@@ -91,6 +101,16 @@ MiniVideoWin.prototype.updateStream = function(stream){
 		break;
 	default:
 		throw new Error("unexpected stream type.");
+	}
+
+	// Safari 浏览器不会自动播放视频，要显示播放按钮
+	if(utils.isSafari && (
+		// 自己的视频、正在播放的视频不显示播放按钮
+		(this.stream && !this.stream.located() && this.stream.paused)
+		// 别人的共享桌面也要显示按钮
+		|| (this.noAudioStream && this.noAudioStream.paused)
+	)){
+		utils.removeClass(this.playButtonDom, "hide");
 	}
 };
 
