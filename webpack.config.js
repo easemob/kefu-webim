13f05abd5744181b4913174d8a481ae99c333071
曹云扬 <caoyy@easemob.com>
2018-02-11 12:03:56 +0800

    format code webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const _en_us_map_ = require("./src/i18n/en-US");
 
 const argv = require("yargs").argv;
 const lang = argv.lang || "zh-CN";
-const tmpVersion = "local_"+(Math.floor(Math.random()*1000000)).toString();
+const tmpVersion = "local_" + (Math.floor(Math.random() * 1e6)).toString();
 const VERSION = argv["tag-name"] || tmpVersion;
 
 var distPath = lang === "zh-CN" ? "" : lang;
