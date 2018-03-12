0115a335f2d305760a078a71e78f11025c5b6785
CW Deng <dcw1123@qq.com>
2018-03-12 16:01:33 +0800

    模板中 ajaxProxyDomain 地址错误

--- server/cfgTemplate.js
+++ server/cfgTemplate.js
@@ -3,11 +3,11 @@ module.exports = function(promptResult){
 	appcfg: {
 		tenantId: "${promptResult.tenantId}",
 		configId: "${promptResult.configId}",
-		ajaxProxyDomain: "sandbox.robot.easemob.com",
+		ajaxProxyDomain: "sandbox.kefu.easemob.com",
 	},
 	servercfg: {
 		port: 8010,
-		proxyto: "sandbox.robot.easemob.com",
+		proxyto: "sandbox.kefu.easemob.com",
 		secure: false,
 	},
 };`;
