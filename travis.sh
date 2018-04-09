bee69889d1c5659c7773a97299eff71d3d6751e6
CW Deng <dcw1123@qq.com>
2018-04-09 19:46:43 +0800

    travis build 没处理 en_US

--- travis.sh
+++ travis.sh
@@ -5,30 +5,30 @@ echo nexus_password: ${nexus_password}
 if [ $TRAVIS_TAG ] && [ "$TRAVIS_TAG"x != ""x ]; then
 
     if [ `echo $TRAVIS_TAG | grep "^plugin_[0-9]\+\.[0-9]\+\.[0-9]\+_final$"` ] || [ `echo $TRAVIS_TAG | grep "^plugin_[0-9]\+\.[0-9]\+\.[0-9]\+_snapshot$"` ]; then
-        
+
         # nexus
         echo ''
         echo '[is a tag] start packing'
         npm config set registry https://registry.npm.taobao.org
-        
+
         npm install
-        npm run build -- --tag-name=$TRAVIS_TAG
+        npm run build_all -- --tag-name=$TRAVIS_TAG
         ls
         cd ../
         ls
-        
-        zip -q -r kefu-webim-${TRAVIS_TAG}.zip kefu-webim -x "kefu-webim/node_modules/*" -x kefu-webim/appPageCached.js 
+
+        zip -q -r kefu-webim-${TRAVIS_TAG}.zip kefu-webim -x "kefu-webim/node_modules/*" -x kefu-webim/appPageCached.js
 
         if [ `echo $TRAVIS_TAG | grep "^plugin_[0-9]\+\.[0-9]\+\.[0-9]\+_final$"` ];then
 
             curl -v -F r=releases -F hasPom=false -F e=zip -F g=com.easemob.kefu.webim.product -F a=kefu-webim -F v=${TRAVIS_TAG} -F p=zip -F file=@${TRAVIS_BUILD_DIR}/../kefu-webim-${TRAVIS_TAG}.zip -u ${nexus_user}:${nexus_password} https://hk.nexus.op.easemob.com/nexus/service/local/artifact/maven/content
-        
+
         else
-            
-            curl -v -F r=releases -F hasPom=false -F e=zip -F g=com.easemob.kefu.webim.development -F a=kefu-webim -F v=${TRAVIS_TAG} -F p=zip -F file=@${TRAVIS_BUILD_DIR}/../kefu-webim-${TRAVIS_TAG}.zip -u ${nexus_user}:${nexus_password} https://hk.nexus.op.easemob.com/nexus/service/local/artifact/maven/content            
+
+            curl -v -F r=releases -F hasPom=false -F e=zip -F g=com.easemob.kefu.webim.development -F a=kefu-webim -F v=${TRAVIS_TAG} -F p=zip -F file=@${TRAVIS_BUILD_DIR}/../kefu-webim-${TRAVIS_TAG}.zip -u ${nexus_user}:${nexus_password} https://hk.nexus.op.easemob.com/nexus/service/local/artifact/maven/content
 
         fi
-     
+
      else
 
         echo ''
@@ -36,7 +36,7 @@ if [ $TRAVIS_TAG ] && [ "$TRAVIS_TAG"x != ""x ]; then
 
     fi
 
-else 
+else
 
 echo ''
 echo '[not a tag] exit packing.'
