a331fa106a4147ad7c1cbc1965064db91d3b1f02
CW Deng <dcw1123@qq.com>
2018-04-27 19:13:12 +0800

    build citic.43.15.5.2

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ todo: delete file_download.png
 
 var debug = false;
 const TEMPLATE_DATA = {
-	WEBIM_PLUGIN_VERSION: 'citic.43.15.5.1'
+	WEBIM_PLUGIN_VERSION: 'citic.43.15.5.2'
 };
 
 const gulp = require('gulp');
