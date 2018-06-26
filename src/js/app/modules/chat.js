ba11f2c850cace4156eff841e0302245e6fdcd7d
fanjinhao <fanfan@easemob.com>
2018-06-26 19:51:27 +0800

    CLOUD-8550 访客端猜你想问（对接百度机器人）前端

--- src/js/app/modules/chat.js
+++ src/js/app/modules/chat.js
@@ -693,8 +693,79 @@ function _bindEvents(){
 			channel.sendText(textMsg);
 			doms.textInput.value = "";
 			utils.trigger(doms.textInput, "change");
+			doms.guessArea.style.display = "none";
+			doms.chatWrapper.style.bottom = 140 + "px";
+			doms.editorView.style.height = 140 + "px";
+			document.getElementById('guessList').innerHTML = "";
+			doms.guessTips.innerText = "guess you say";
+			doms.loading.style.display = "block";
 		}
 	});
+
+	// 文本框检索猜你想说内容
+	utils.on(doms.textInput, "keyup", function(){
+		doms.guessArea.style.display = "block";
+		var value = this.value;
+		// 根据当前文本框输入内容 是否发起请求
+		if(value != ""){
+			apiHelper.getGuessList(value).then(function(res){
+				if(res && res.data && res.data.entities){
+					doms.guessTips.innerText = "guess you say";
+					doms.loading.style.display = "none";
+					// 创建模板
+					createTemplate(res.data.entities);
+					// 设置聊天框内容样式
+					doms.chatWrapper.style.bottom = 230 + "px";
+					doms.editorView.style.height = 230 + "px";
+					doms.guessBtn = document.querySelectorAll('.guess-list li');
+					// 点击猜你想说按钮上屏
+					utils.on(doms.guessBtn, "click", function(){
+						var curText = this.innerText;
+						channel.sendText(curText);
+						doms.textInput.value = "";
+						utils.trigger(doms.textInput, "change");
+						// 恢复默认样式
+						doms.guessArea.style.display = "none";
+						doms.chatWrapper.style.bottom = 140 + "px";
+						doms.editorView.style.height = 140 + "px";
+						document.getElementById('guessList').innerHTML = "";
+					});
+				}else{
+					// 检索不到值时恢复 默认样式
+					doms.guessTips.innerText = "暂无查询结果";
+					doms.loading.style.display = "block";
+					document.getElementById('guessList').innerHTML = "";
+					doms.chatWrapper.style.bottom = 140 + "px";
+					doms.editorView.style.height = 140 + "px";
+				}
+			});
+		}
+		else{
+			// 文本框内容清空时 重置样式
+			doms.guessArea.style.display = "none";
+			doms.chatWrapper.style.bottom = 140 + "px";
+			doms.editorView.style.height = 140 + "px";
+		}
+	});
+
+	// 点击猜你想说关闭按钮 恢复样式
+	utils.on(doms.closeBtn, "click", function(){
+		doms.guessArea.style.display = "none";
+		doms.chatWrapper.style.bottom = 140 + "px";
+		doms.editorView.style.height = 140 + "px";
+		doms.guessTips.innerText = "guess you say";
+		doms.loading.style.display = "block";
+	});
+
+}
+
+// 根据`猜你想问`接口返回拼接模板
+function createTemplate(data){
+		var html="";
+		for(var i=0; i<data.length; i++){
+			html+='<li>' + data[i] + '</li>';
+			document.getElementById('guessList').innerHTML = html;
+		}
 }
 
 function setArticleIframeScrolling(enable){
@@ -781,6 +852,13 @@ function _getDom(){
 		minifyBtn: topBar.querySelector(".btn-min"),
 		audioBtn: topBar.querySelector(".btn-audio"),
 		switchKeyboardBtn: topBar.querySelector(".btn-keyboard"),
+
+		guessTips: editorView.querySelector(".guess-tips"),
+		guessArea: editorView.querySelector(".guess-area"),
+		closeBtn: editorView.querySelector(".close-btn"),
+		guessBtn: editorView.querySelectorAll(".guess-area li"),
+		guessList: editorView.querySelector(".guess-list"),
+		loading: editorView.querySelector(".loading"),
 
 		emojiToggleButton: editorView.querySelector(".em-bar-emoji"),
 		sendImgBtn: editorView.querySelector(".em-widget-img"),
