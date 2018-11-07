10bea99dee8bcb236f070113439fb2faa05c8117
fanjinhao <fanfan@easemob.com>
2018-11-07 18:32:02 +0800

    CLOUD-11430 前端：CEC web访客端聊天窗口下加上 power by环信及超链

--- src/js/app/modules/apiHelper.js
+++ src/js/app/modules/apiHelper.js
@@ -1248,6 +1248,17 @@ function getGuessList(data){
 	});
 }
 
+// 获取serviceLevel 字段
+function getServiceLevel(){
+	return new Promise(function(resolve, reject){
+		api("getServiceLevel", {
+			tenantId: config.tenantId,
+		}, function(msg){
+			resolve(msg);
+		});
+	});
+}
+
 module.exports = {
 	getCurrentServiceSession: getCurrentServiceSession,
 	getToken: getToken,
@@ -1298,6 +1309,7 @@ module.exports = {
 	updateCustomerInfo: updateCustomerInfo,
 	getArticleJson: getArticleJson,
 	getGuessList: getGuessList,
+	getServiceLevel: getServiceLevel,
 
 	initApiTransfer: initApiTransfer,
 	api: api,
