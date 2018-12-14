74a1c2b118c16c26387ad21141c115e6eb8b6c79
Pros <8038203@qq.com>
2018-12-14 14:14:04 +0800

    build citic.43.15.10

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ todo: delete file_download.png
 
 var debug = false;
 const TEMPLATE_DATA = {
-	WEBIM_PLUGIN_VERSION: 'citic.43.15.9'
+	WEBIM_PLUGIN_VERSION: 'citic.43.15.10'
 };
 
 const gulp = require('gulp');
