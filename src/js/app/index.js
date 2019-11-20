87f49f91d35e1999499d479a6d660a28551e4849
zhaoyan <963873389@qq.com>
2019-11-20 11:21:58 +0800

    h5 模式聊天窗口／自主服务各占一半宽度

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -307,6 +307,9 @@ function renderUI(resultStatus){
 	else if(commonConfig.getConfig().configId){
 		if(!utils.isMobile){
 			utils.addClass(document.body, "big-window");
+			if(utils.isTop){
+				utils.addClass(document.body, "big-window-h5");
+			}
 		}
 		// 全部渲染
 		if(!utils.isMobile && (resultStatus[0] || resultStatus[1])){
@@ -314,9 +317,11 @@ function renderUI(resultStatus){
 			functionView.init({
 				resultStatus: resultStatus
 			});
-			// 常见问题和自主服务，固定宽度 360px
-			let dialogWidth = (Math.floor(commonConfig.getConfig().dialogWidth.slice(0, -2)) + Math.floor(360)) + "px";
-			handleSettingIframeSize({ width: dialogWidth });
+			// iframe形式 常见问题和自主服务，固定宽度 360px
+			if(!utils.isTop){
+				let dialogWidth = (Math.floor(commonConfig.getConfig().dialogWidth.slice(0, -2)) + Math.floor(360)) + "px";
+				handleSettingIframeSize({ width: dialogWidth });
+			}
 		}
 		// 常见问题和自助服务开关都关闭时
 		else if(!resultStatus[0] && !resultStatus[1]){
