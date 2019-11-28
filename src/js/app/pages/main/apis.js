01d81f888f4372ff64724eaa9942ff46c5672863
zhaoyan <963873389@qq.com>
2019-11-28 12:45:31 +0800

    参数转译

--- src/js/app/pages/main/apis.js
+++ src/js/app/pages/main/apis.js
@@ -152,7 +152,7 @@ function getNoteCategories(){
 			api("getNoteCategories", {
 				tenantId: config.tenantId,
 				"easemob-target-username": config.toUser,
-				"easemob-appkey": config.appKey,
+				"easemob-appkey": config.appKey.replace("#", "%23"),
 				"easemob-username": config.user.username,
 				headers: { Authorization: "Easemob IM " + token },
 				projectId: projectId,
@@ -172,7 +172,7 @@ function createTicket(opt){
 		api("createTicket", {
 			tenantId: config.tenantId,
 			"easemob-target-username": config.toUser,
-			"easemob-appkey": config.appKey,
+			"easemob-appkey": config.appKey.replace("#", "%23"),
 			"easemob-username": config.user.username,
 			config_id: config.configId,
 			config_name: config.configName,
