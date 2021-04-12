b1148e19f4b86680d944d98428945e0406a32dbf
lyj <17637515819@163.com>
2021-04-12 19:56:12 +0800

    66639 租户特殊处理 多端登录问题

--- src/js/app/pages/main/channel.js
+++ src/js/app/pages/main/channel.js
@@ -168,6 +168,10 @@ function _initConnection(onReadyCallback){
 			}
 			// 当多端登录挤掉上一个ws链接的时候给出提示
 			if(e.type === 8){
+				
+				if(config.tenantId == "66639"){
+					return false;
+				}
 				_refreshDialog();
 				clearInterval(receiveMsgTimer);
 				eventListener.trigger(_const.SYSTEM_EVENT.CHAT_CLOSED);
