d9acbd858f7b5c6c46de9626d36e6d8b354fa163
lyj <17637515819@163.com>
2021-04-27 13:33:55 +0800

    没有发起聊天时候重新连接后消息没有渲染

--- src/js/app/pages/main/channel.js
+++ src/js/app/pages/main/channel.js
@@ -19,6 +19,7 @@ var config;
 var conn;
 var evaluateTime;
 var evaluateFlag = false;
+// var isPullHistory = false;
 
 
 // 监听ack的timer, 每条消息启动一个
@@ -181,6 +182,7 @@ function _initConnection(onReadyCallback){
 				eventListener.trigger(_const.SYSTEM_EVENT.CHAT_CLOSED);
 				eventListener.trigger(_const.SYSTEM_EVENT.CLEAR_AGENTSTATE);
 				eventListener.trigger(_const.SYSTEM_EVENT.CLEAR_AGENTINPUTSTATE);
+				eventListener.trigger(_const.SYSTEM_EVENT.IS_PULL_HISTORY);
 			}
 		}
 	});
@@ -196,6 +198,10 @@ function _initConnection(onReadyCallback){
 			evaluateTime = 8*3600
 		}
 	});
+	// 添加一个监听事件判断是否调用历史消息
+	eventListener.add(_const.SYSTEM_EVENT.IS_PULL_HISTORY, function(){
+		receiveMsgDict = new Dict();
+	})
 }
 function _refreshDialog(){
 	var modelDom = utils.createElementFromHTML("<div class=\"em-model\"></div>");
