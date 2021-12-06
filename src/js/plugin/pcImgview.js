89d1721ceafce02efef516f77cd23e01bbb9ec06
王彬 <thresh.wang@easemob.com>
2021-12-06 17:52:39 +0800

    fix: 修复 ipad 预览图片的 bug

--- src/js/plugin/pcImgview.js
+++ src/js/plugin/pcImgview.js
@@ -18,31 +18,25 @@ function _init(){
 	}, false);
 }
 
-function formatImg(file) {
-	var reader = new FileReader(); //实例化文件读取对象
-	reader.readAsDataURL(file); //将文件读取为 DataURL,也就是base64编码
-	reader.onload = function(ev) { //文件读取成功完成时触发
-		var dataURL = ev.target.result; //获得文件读取成功后的DataURL,也就是base64编码
-		console.log('dataURL', dataURL);
-		return dataURL
-	}
-	console.log('file', file);
-}
-
-module.exports = function(imgData){
+module.exports = function(imgData) {
 	console.log('imgData', imgData)
 	var imgFile = imgData.imgFile;
 
-	if(!isInitialized){
+	if (!isInitialized) {
 		_init();
 		isInitialized = true;
 	}
 
-	if(imgFile){
+	if (imgFile) {
 		// imgDom.src = window.URL.createObjectURL(imgFile);
-		imgDom.src = formatImg(imgFile)
-	}
-	else{
+		// 转 base64
+		var reader = new FileReader(); //实例化文件读取对象
+		reader.readAsDataURL(imgFile); //将文件读取为 DataURL,也就是base64编码
+		reader.onload = function(ev) { //文件读取成功完成时触发
+			var dataURL = ev.target.result; //获得文件读取成功后的DataURL,也就是base64编码
+			imgDom.src = dataURL
+		}
+	} else {
 		imgDom.src = imgData.imgSrc;
 	}
 	console.log('imgDom.src：', imgDom.src)
