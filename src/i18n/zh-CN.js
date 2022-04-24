c5b26cc38d02e06a5645c91e20001f02f47a4b54
lyj <17637515819@163.com>
2022-04-24 10:38:37 +0800

    CLOUD-27284【VEC1.1】web访客端，视频通话中文字修改为通话中和坐席端统一

--- src/i18n/zh-CN.js
+++ src/i18n/zh-CN.js
@@ -123,7 +123,7 @@ module.exports = {
 		invite_agent_video: "邀请客服进行实时视频",
 		invite_exit_video: "访客取消实时视频",
 		invite_reject_video: "访客拒绝实时视频",
-		connecting: "视频通话中",
+		connecting: "通话中",
 		waiting_confirm: "视频连接请求，等待你的确认",
 		can_not_connected: "视频无法接通，请重试",
 		can_not_open_camera: "打开摄像头失败",
