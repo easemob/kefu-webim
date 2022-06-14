5b0d3a7b72d93cd131c5040550ec5bbc7347e05e
lyj <17637515819@163.com>
2022-06-14 14:31:07 +0800

    不调用IM接口时候也不调用初始化语种接口 (reverted from commit d34a25d0b87052f1a9edaabb05c5deb716108f86)

--- src/js/app/pages/main/init.js
+++ src/js/app/pages/main/init.js
@@ -217,12 +217,14 @@ function initChatEntry(targetUserInfo){
 			break;
 
 		case "userWidthNameAndPassword":
+			_initLanuage(commonConfig.getConfig().user);
 			profile.imPassword = commonConfig.getConfig().user.password;
 			chat.init();
 			break;
 
 		case "wechatAuth":
 		case "widthPassword":
+			_initLanuage(commonConfig.getConfig().user);
 			chat.init();
 			break;
 
@@ -298,6 +300,7 @@ function userWithPasswordEntry(targetUserInfo){
 function userNameAndTokenEntry(){
 	// todo: move imToken to an independent key
 	profile.imToken = commonConfig.getConfig().user.token;
+	_initLanuage(commonConfig.getConfig().user);
 	chat.init();
 }
 
