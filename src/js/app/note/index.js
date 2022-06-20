a0d36801c664450082d9281f7021149c58499379
chengxiuning <2292005044@qq.com>
2022-06-20 10:11:36 +0800

    海外集群租户使用网页插件留言,不用验证手机号

--- src/js/app/note/index.js
+++ src/js/app/note/index.js
@@ -27,13 +27,13 @@ var dom = utils.createElementFromHTML([
 	"<textarea spellcheck=\"false\" placeholder=\"" + __("ticket.content_placeholder") + "\"></textarea><span class=\"font-red-text\">*</span>",
 	"<div class=\"line\"></div>",
 	"<div class=\"cancel\">" + __("common.cancel") + "</div>",
-	"<div class=\"confirm bg-color\">"+ __("common.ticket") +"</div>",
+	"<div class=\"confirm bg-color\">" + __("common.ticket") + "</div>",
 	"</div>",
 	"</div>"
 ].join(""));
 
 // 不要用parent 获取元素。
-if(utils.isMobile  || (config.showBody && config.showSelf)){
+if (utils.isMobile || (config.showBody && config.showSelf)) {
 	var dom = utils.createElementFromHTML([
 		"<div  class=\"em-dialog note\">",
 		"<div class=\"wrapper\">",
@@ -46,12 +46,12 @@ if(utils.isMobile  || (config.showBody && config.showSelf)){
 		"<textarea spellcheck=\"false\" placeholder=\"" + __("ticket.content_placeholder") + "\"></textarea><span class=\"font-red-text\">*</span>",
 		"<div class=\"line\"></div>",
 		"<div class=\"cancel\">" + __("common.cancel") + "</div>",
-		"<div class=\"confirm bg-color\">"+ __("common.ticket") +"</div>",
+		"<div class=\"confirm bg-color\">" + __("common.ticket") + "</div>",
 		"</div>",
 		"</div>"
 	].join(""));
 }
-else if(config.showBody && !config.showSelf){
+else if (config.showBody && !config.showSelf) {
 	var dom = utils.createElementFromHTML([
 		"<div id=\"demo-wrapper\" class=\"em-dialog ticket satisfaction \">",
 		"<div class=\"wrapper\">",
@@ -64,7 +64,7 @@ else if(config.showBody && !config.showSelf){
 		"<textarea spellcheck=\"false\" placeholder=\"" + __("ticket.content_placeholder") + "\"></textarea><span class=\"font-red-text\">*</span>",
 		"<div class=\"line\"></div>",
 		"<div class=\"cancel\">" + __("common.cancel") + "</div>",
-		"<div class=\"confirm bg-color\">"+ __("common.ticket") +"</div>",
+		"<div class=\"confirm bg-color\">" + __("common.ticket") + "</div>",
 		"</div>",
 		"</div>"
 	].join(""));
@@ -91,38 +91,38 @@ utils.addClass(document.body, config.themeClassName || "theme-1");
 config.hideCloseBtn && utils.addClass(cancelBtn, "hide");
 
 var themeCustomColor = config.themeCustomColor;
-if(themeCustomColor){
+if (themeCustomColor) {
 	// color = config.themeColor;
-	$(".theme_custom").find(".bg-color").css("cssText","background-color: " + themeCustomColor + " !important");
+	$(".theme_custom").find(".bg-color").css("cssText", "background-color: " + themeCustomColor + " !important");
 }
 var color = config.themeColor;
-if(color){
-	$(".theme_custom").find(".bg-color").css("cssText","background-color: " + color + " !important");
+if (color) {
+	$(".theme_custom").find(".bg-color").css("cssText", "background-color: " + color + " !important");
 }
-if(config.offDutyType == ""){
+if (config.offDutyType == "") {
 	var closeEl = $(".ticket>.wrapper .icon-close");
 	var ticketEl = $(".ticket>.wrapper .confirm");
-	closeEl&&closeEl.remove();
-	$(ticketEl).css("cssText","margin-left: 280px;display:block;margin:auto;margin-bottom: 65px;");
+	closeEl && closeEl.remove();
+	$(ticketEl).css("cssText", "margin-left: 280px;display:block;margin:auto;margin-bottom: 65px;");
 }
 
-var _getCategories = _.once(function(){
-	api.getNoteCategories().then(function(list){
-		var optionList = _.map(list, function(item){
+var _getCategories = _.once(function () {
+	api.getNoteCategories().then(function (list) {
+		var optionList = _.map(list, function (item) {
 			return {
 				sign: item.id,
 				desc: item.name
 			};
 		});
 
-		if(!_.isEmpty(optionList)){
+		if (!_.isEmpty(optionList)) {
 			utils.removeClass(noteCategory, "hide");
 			noteCategoryList.updateList({ list: optionList });
 		}
 	});
 });
 
-function getNoteConfig(){
+function getNoteConfig() {
 	var i;
 	var len;
 	var tmp = [];
@@ -130,7 +130,7 @@ function getNoteConfig(){
 	// 需要测兼容性
 	var src = document.location.search;
 	var arr = src.slice(1).split("&");
-	for(i = 0, len = arr.length; i < len; i++){
+	for (i = 0, len = arr.length; i < len; i++) {
 		tmp = arr[i].split("=");
 		tmp[1] = window.atob
 			? window.atob(tmp[1])
@@ -142,11 +142,11 @@ function getNoteConfig(){
 	return obj;
 }
 
-function _createTicket(){
+function _createTicket() {
 	Promise.all([
 		api.getToken(),
 		api.getProjectId()
-	]).then(function(result){
+	]).then(function (result) {
 		var token = result[0];
 		var projectId = result[1];
 		var sessionId = config.sessionId || "";
@@ -160,31 +160,31 @@ function _createTicket(){
 			content: content.value,
 			category_id: noteCategoryList.getSelectedValue(),
 			session_id: sessionId,
-		}).then(function(){
+		}).then(function () {
 			isSending = false;
 			uikit.showSuccess(__("ticket.send_success"));
 
 			_clearInput();
-		}, function(err){
+		}, function (err) {
 			isSending = false;
 			uikit.tip(__("ticket.send_failed_retry"));
 			console.error(err);
 		});
 	})
-	["catch"](function(err){
+	["catch"](function (err) {
 		uikit.tip(__("ticket.send_failed_invalid_token"));
 		console.error(err);
 	});
 }
 
-function _clearInput(){
+function _clearInput() {
 	name.value = "";
 	phone.value = "";
 	mail.value = "";
 	content.value = "";
 }
 
-function _writePreDate(preData){
+function _writePreDate(preData) {
 	name.value = preData.name || "";
 	phone.value = preData.phone || "";
 	mail.value = preData.mail || "";
@@ -195,81 +195,85 @@ config.preData && _writePreDate(config.preData);
 
 
 // 海外用户，不验证手机号格式 新增敦煌用户不校验格式2020-05-06
-function overseasTest(){
-	if(config.tenantId == "76141"){
+function overseasTest() {
+	// 海外集群
+	if (config.tenantId >= 1000000) {
 		return !phone.value;
 	}
-	if(config.tenantId == "78882"){
+	if (config.tenantId == "76141") {
 		return !phone.value;
 	}
-	if(config.tenantId == "6437"){
-        return !phone.value;
-    }
+	if (config.tenantId == "78882") {
+		return !phone.value;
+	}
+	if (config.tenantId == "6437") {
+		return !phone.value;
+	}
 	return !phone.value || !(/^1[3456789]\d{9}$/.test(phone.value));
 }
 // 添加邮箱的校验
-function checkEmail(){
+function checkEmail() {
 	var check = /^[0-9a-z]([_.0-9a-z-]{0,30}[0-9a-z])?@([0-9a-z][0-9a-z-]{0,30}[.]){1,3}[a-z]{2,4}$/i;
-	if(mail.value){
-		if(check.test(mail.value)){
+	if (mail.value) {
+		if (check.test(mail.value)) {
 			return true
-		}else{
+		} else {
 			return false
 		}
 	}
-	else{
+	else {
 		return true
 	}
 }
 
 // 留言
-utils.on(confirmBtn, "click", function(){
-	if(isSending){
+utils.on(confirmBtn, "click", function () {
+	if (isSending) {
 		uikit.tip(__("ticket.is_sending"));
 	}
-	else if(!name.value || name.value.length > 140){
+	else if (!name.value || name.value.length > 140) {
 		uikit.tip(__("ticket.invalid_name"));
 	}
-	else if(overseasTest()){
+	else if (overseasTest()) {
 		uikit.tip(__("ticket.invalid_phone"));
 	}
-	else if(!checkEmail()){
+	else if (!checkEmail()) {
 		uikit.tip(__("ticket.invalid_email"));
 	}
-	else if(!content.value || content.value.length > 1500){
+	else if (!content.value || content.value.length > 1500) {
 		uikit.tip(__("ticket.invalid_content"));
 	}
-	else{
+	else {
 		isSending = true;
-		setTimeout(function(){ isSending = false; }, 10000);
+		setTimeout(function () { isSending = false; }, 10000);
 		_createTicket();
 	}
 });
 
 // 取消
-utils.on(cancelBtn, "click", function(){
-	if(config.offDutyType == ""){
-		
+utils.on(cancelBtn, "click", function () {
+	if (config.offDutyType == "") {
+
 	}
-	else{
+	else {
 		window.parent.postMessage({ closeNote: true }, "*");
 	}
 });
-utils.live(".wrapper-title .icon-close","click",function(){
+utils.live(".wrapper-title .icon-close", "click", function () {
 	// window.parent.postMessage({ closeNote: true }, "*");
-	if(config.offDutyType == ""){
-		
+	if (config.offDutyType == "") {
+
 	}
-	else{
+	else {
 		window.parent.postMessage({ closeNote: true }, "*");
 	}
 });
-utils.live(".wrapper-title .icon-back-new","click",function(){
+utils.live(".wrapper-title .icon-back-new", "click", function () {
 	// window.parent.postMessage({ closeNote: true }, "*");
-	if(config.offDutyType == ""){
+	if (config.offDutyType == "") {
 		window.parent.postMessage({ closeChat: true }, "*");
 	}
-	else{
+	else {
 		window.parent.postMessage({ closeNote: true }, "*");
 	}
 });
